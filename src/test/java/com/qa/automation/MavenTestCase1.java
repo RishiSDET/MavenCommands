a238a8590670bb7f973877aacdb822ce040ca1e6
Rishi Bhargava <bhargavarishi1989@gmail.com>
2023-05-27 19:26:52 +0530

    Fist Commit for github  jenkins

---
+++ src/test/java/com/qa/automation/MavenTestCase1.java
@@ -0,0 +1,19 @@
+package com.qa.automation;
+
+import org.testng.annotations.Test;
+
+public class MavenTestCase1 {
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("This is the First Method under Maven project Test Case 1");
+	}
+	
+	@Test
+	public void m2()
+	{
+		System.out.println("This is the Second Method under Maven project Test Case 1");
+	}
+
+}
---
+++ src/test/java/com/qa/automation/MavenTestCase2.java
@@ -0,0 +1,19 @@
+package com.qa.automation;
+
+import org.testng.annotations.Test;
+
+public class MavenTestCase2 {
+	
+	@Test
+	public void m3()
+	{
+		System.out.println("This is the Third Method under Maven project Test Case 2");
+	}
+	
+	@Test
+	public void m4()
+	{
+		System.out.println("This is the Fourth Method under Maven project Test Case 2");
+	}
+
+}
